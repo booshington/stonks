c6c57733cef8148ad16dc54851a44d6c87414f88
Kyle Bouchard <kyleisagod@gmail.com>
2020-11-19 13:11:34 -0500

    Added empty component, removed initial create-react-app references and comments, updated testing.

---
+++ src/Components/StockTicker/StockTicker.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StockTicker = () => {
+    return (
+        <div>
+            <p>
+                Ticker
+            </p>
+        </div>
+    )
+}
+
+export default StockTicker;
