4c93811c8da892811628461a46fed327fa989c44
booshington <kyleisagod@gmail.com>
2020-12-26 22:00:30 -0500

    Removed unirest as it did not work properly. Added finnhub as it does work properly. Added api key and example code per documentation.

--- src/Components/StockTicker/StockTicker.tsx
+++ src/Components/StockTicker/StockTicker.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { StockTickerProps, StockTickerState } from './StockTicker.types';
+import * as Config from '../../Config';
 
 class StockTicker extends React.Component<StockTickerProps, StockTickerState>{
     constructor(props: StockTickerProps) {
@@ -8,6 +9,17 @@ class StockTicker extends React.Component<StockTickerProps, StockTickerState>{
         this.state = {
             symbol: props.symbol,   
         };
+    }
+
+    componentDidMount(){
+        const finnhub = require('finnhub');
+        const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+        api_key.apiKey = Config.apiKey
+        const finnhubClient = new finnhub.DefaultApi()
+        //TODO: Replace error, data, and response types from any to their proper types
+        finnhubClient.stockCandles(this.state.symbol, "D", 1590988249, 1591852249, {}, (error:any, data:any, response:any) => {
+            console.log(data)
+        });
     }
 
     render() {
