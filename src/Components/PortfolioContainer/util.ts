c1b2d4ef3f82f26a9b1cc9d378f3615b609b885a
boosh <boosh@booshdevlaptop.localdomain>
2021-02-02 18:12:00 -0500

    Added default state value of portfolio to fix a compilation error

--- src/Components/PortfolioContainer/util.ts
+++ src/Components/PortfolioContainer/util.ts
@@ -1,6 +1,7 @@
 import { PortfolioContainerState } from "./PortfolioContainer.types";
 
 export const defaultProps:PortfolioContainerState = {
+    portfolio: {},
     symbols: ["TSLA", "AAPL"],
     data:[],
 }
